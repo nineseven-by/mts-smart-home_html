2ca0bcccaaad9f7bf5acbb471f99207736fbb254
Luka Nikitin <x0333@pm.me>
2024-03-29 09:45:26 +0300

    wip

--- sources/src/js/back.js
+++ sources/src/js/back.js
@@ -1,3 +1,49 @@
 import $ from "jquery";
 
-$(".wrapper").addClass("jquery-ready"); // This is example string, remove pls for prod
+$(function() {
+
+    $('#submitable-form__footer').on('submit', (e) => {
+        e.preventDefault();
+
+        const currentFormData = $('#submitable-form__footer').serializeArray();
+          
+        $.ajax({
+            "url": "http://localhost:3500/request-mailing",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "data": {
+                "email": currentFormData.find(({ name }) => name === 'client-email')?.value,
+                "news": currentFormData.find(({ name }) => name === 'sub-news')?.value === 'on' ? 'true' : 'false',
+                "promotions": currentFormData.find(({ name }) => name === 'sub-promo')?.value === 'on' ? 'true' : 'false'
+            }
+        })
+
+        $('#submitable-form__footer .subscribe__submit').text('Вы успешно подписались!');
+        $('#submitable-form__footer .subscribe__submit').attr('disabled', true);
+    })
+
+
+    $('.smarthome-order__form').on('submit', (e) => {
+        e.preventDefault();
+
+        const form = $('.smarthome-order__form');
+        const commentText = $('#comment').val().trim();
+
+        $.ajax({
+            "url": "http://localhost:3500/request-consultation",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "data": {
+                "phone": $('#phone').val(),
+                "name": $('#client').val(),
+                "comment": commentText === '' ? null : commentText,
+            }
+        })
+
+        $('.smarthome-order__form').html('<span style="margin-top:-25px;">Заявка успешно отправлена!</span>');
+    })
+})
